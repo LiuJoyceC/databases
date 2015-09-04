de0d8f6fa9358e1f369c20fb64b54c874f50de3a
remotebeta-robot <chris.schepman+remotebeta-robot@hackreactor.com>
2015-09-04 17:58:09 +0000

    (init)

---
+++ orm-resources/orm-example.js
@@ -0,0 +1,42 @@
+/* You'll need to
+ * npm install sequelize
+ * before running this example. Documentation is at http://sequelizejs.com/
+ */
+
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("chatter", "root", "");
+/* TODO this constructor takes the database name, username, then password.
+ * Modify the arguments if you need to */
+
+/* first define the data structure by giving property names and datatypes
+ * See http://sequelizejs.com for other datatypes you can use besides STRING. */
+var User = sequelize.define('User', {
+  username: Sequelize.STRING
+});
+
+var Message = sequelize.define('Message' {
+  userid: Sequelize.INTEGER,
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
+
+/* .sync() makes Sequelize create the database table for us if it doesn't
+ *  exist already: */
+User.sync().success(function() {
+  /* This callback function is called once sync succeeds. */
+
+  // now instantiate an object and save it:
+  var newUser = User.build({username: "Jean Valjean"});
+  newUser.save().success(function() {
+
+    /* This callback function is called once saving succeeds. */
+
+    // Retrieve objects from the database:
+    User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
+      // This function is called back with an array of matches.
+      for (var i = 0; i < usrs.length; i++) {
+        console.log(usrs[i].username + " exists");
+      }
+    });
+  });
+});
---
+++ server/controllers/index.js
@@ -0,0 +1,18 @@
+var models = require('../models');
+var bluebird = require('bluebird');
+
+
+
+module.exports = {
+  messages: {
+    get: function (req, res) {}, // a function which handles a get request for all messages
+    post: function (req, res) {} // a function which handles posting a message to the database
+  },
+
+  users: {
+    // Ditto as above
+    get: function (req, res) {},
+    post: function (req, res) {}
+  }
+};
+
